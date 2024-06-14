fc16e2d45028e8218f3ecd987122c1a856437219
Shoghy Martinez <54950187+Shoghy@users.noreply.github.com>
2024-06-14 09:12:12 -0400

    ♻Reorganizing option tests

--- tests/option.test.ts
+++ tests/option.test.ts
@@ -224,147 +224,177 @@ describe("Testing `and` method", () => {
   });
 });
 
-test("`and_then` should not execute ", () => {
-  const none = None<number>();
-  const result = none.and_then((value) => {
-    return Some(value * value);
+describe("Testing `and_then` method", () => {
+  test("`None` should not execute", () => {
+    const none = None<number>();
+    const result = none.and_then((value) => {
+      return Some(value * value);
+    });
+    expect(result).toEqual(None());
   });
-  expect(result).toEqual(None());
-});
 
-test("`and_then` should execute ", () => {
-  const some = Some(5);
-  const result = some.and_then((value) => {
-    return Some(value * value);
+  test("`Some` should execute", () => {
+    const some = Some(5);
+    const result = some.and_then((value) => {
+      return Some(value * value);
+    });
+    expect(result).toEqual(Some(25));
   });
-  expect(result).toEqual(Some(25));
 });
 
-test("`get_or_insert` should change the value of `None`", () => {
-  const option = None<number>();
-  const result = option.get_or_insert(3.1415);
-  expect(result).toBe(3.1415);
-  expect(option).toEqual(Some(3.1415));
+describe("Testing `get_or_insert` method", () => {
+  test("`None` should insert the value", () => {
+    const option = None<number>();
+    const result = option.get_or_insert(3.1415);
+    expect(result).toBe(3.1415);
+    expect(option).toEqual(Some(3.1415));
+  });
+
+  test("`Some` should not insert the value", () => {
+    const option = Some(42);
+    const result = option.get_or_insert(9+10); //21
+    expect(result).toBe(42);
+    expect(option).toEqual(Some(42));
+  });
 });
 
-test("`get_or_insert` should not change the value of `Some`", () => {
-  const option = Some(42);
-  const result = option.get_or_insert(9+10); //21
-  expect(result).toBe(42);
-  expect(option).toEqual(Some(42));
+describe("Testing `get_or_insert_with` method", () => {
+  test("`get_or_insert_with` should change the value of `None`", () => {
+    const option = None<string[]>();
+    const result = option.get_or_insert_with(
+      () => Array.from("Hello World!")
+    );
+    expect(result).toEqual(Array.from("Hello World!"));
+    expect(option).toEqual(Some(Array.from("Hello World!")));
+  });
+
+  test("`get_or_insert_with` should not change the value of `Some`", () => {
+    const option = Some("Cards Against Humanity");
+    const result = option.get_or_insert_with(
+      () => "Humanity"
+    );
+    expect(result).toEqual("Cards Against Humanity");
+    expect(option).toEqual(Some("Cards Against Humanity"));
+  });
 });
 
-test("`get_or_insert_with` should change the value of `None`", () => {
-  const option = None<string[]>();
-  const result = option.get_or_insert_with(
-    () => Array.from("Hello World!")
-  );
-  expect(result).toEqual(Array.from("Hello World!"));
-  expect(option).toEqual(Some(Array.from("Hello World!")));
+describe("Testing `insert` method", () => {
+  test("`insert` should change the value of `None`", () => {
+    const cool_song = "https://open.spotify.com/track/4S3dFI8Sx3UsKOUnoYFCg2";
+
+    const option1 = None<string>();
+    const result1 = option1.insert(cool_song);
+    expect(result1).toBe(cool_song);
+    expect(option1).not.toEqual(None());
+    expect(option1).toEqual(Some(cool_song));
+  });
+
+  test("`insert` should change the value of `Some`", () => {
+    const option2 = Some(1);
+    const result2 = option2.insert(2);
+    expect(result2).toBe(2);
+    expect(option2).not.toEqual(Some(1));
+    expect(option2).toEqual(Some(2));
+  });
 });
 
-test("`get_or_insert_with` should not change the value of `Some`", () => {
-  const option = Some("Cards Against Humanity");
-  const result = option.get_or_insert_with(
-    () => "Humanity"
-  );
-  expect(result).toEqual("Cards Against Humanity");
-  expect(option).toEqual(Some("Cards Against Humanity"));
-});
-
-test("`insert` should change the value of `Option`", () => {
-  const cool_song = "https://open.spotify.com/track/4S3dFI8Sx3UsKOUnoYFCg2";
-
-  const option1 = None<string>();
-  const result1 = option1.insert(cool_song);
-  expect(result1).toBe(cool_song);
-  expect(option1).not.toEqual(None());
-  expect(option1).toEqual(Some(cool_song));
-
-  const option2 = Some(1);
-  const result2 = option2.insert(2);
-  expect(result2).toBe(2);
-  expect(option2).not.toEqual(Some(1));
-  expect(option2).toEqual(Some(2));
-});
-
-test("`is_some_and` should be false if `Option` is `None`", () => {
+describe("Testing `is_some_and` method", () => {
   const r_true = () => true;
   const r_false = () => false;
 
-  const none = None();
+  test("`None` should always return false", () => {
+    const none = None();
 
-  expect(none.is_some_and(r_true)).toBe(false);
-  expect(none.is_some_and(r_false)).toBe(false);
+    expect(none.is_some_and(r_true)).toBe(false);
+    expect(none.is_some_and(r_false)).toBe(false);
+  });
+
+  test("`Some` should execute the function and return its value", () => {
+    const some = Some(1);
+
+    expect(some.is_some_and(r_true)).toBe(true);
+    expect(some.is_some_and(r_false)).toBe(false);
+    expect(some.is_some_and((value) => value == 1)).toBe(true);
+    expect(some.is_some_and((value) => value == 2)).toBe(false);
+  });
 });
 
-test("testing `is_some_and` with `Some`", () => {
-  const r_true = () => true;
-  const r_false = () => false;
+describe("Testing `take` method", () => {
+  test("`Some` should transform into `None`", () => {
+    const option = Some(142857);
+    option.take();
+    expect(option).toEqual(None());
+  });
 
-  const some = Some(1);
+  test("`Some` should return its value wrapped in a `Some`", () => {
+    const option1 = Some("0xff");
+    const option2 = option1.take();
+    expect(option2).toEqual(Some("0xff"));
+  });
 
-  expect(some.is_some_and(r_true)).toBe(true);
-  expect(some.is_some_and(r_false)).toBe(false);
-  expect(some.is_some_and((value) => value == 1)).toBe(true);
-  expect(some.is_some_and((value) => value == 2)).toBe(false);
+  test("`None` should be left unchanged", () => {
+    const none = None();
+    none.take();
+    expect(none).toEqual(none);
+  });
+
+  test("`None` should return `None`", () => {
+    const none1 = None();
+    const none2 = none1.take();
+    expect(none2).toEqual(None());
+  });
 });
 
-test("testing `take` method", () => {
-  let option1 = Some(142857);
-  let option2 = option1.take();
+describe("Testing `unwrap_or` method", () => {
+  test("`None` should return the `def` value", () => {
+    const none = None<string>();
+    const result = none.unwrap_or("31 minutos");
+    expect(result).toBe("31 minutos");
+  });
 
-  expect(option1).toEqual(None());
-  expect(option2).toEqual(Some(142857));
-
-  option2 = option1.take();
-  expect(option1).toEqual(None());
-  expect(option2).toEqual(None());
-
-  option1 = None();
-  option2 = option1.take();
-  expect(option1).toEqual(None());
-  expect(option2).toEqual(None());
+  test("`Some` should return its own value", () => {
+    const some = Some("Mr. Trance");
+    const result = some.unwrap_or("Esteman");
+    expect(result).toBe("Mr. Trance");
+  });
 });
 
-test("`unwrap_or` should be called ", () => {
-  const none = None<string>();
-  const result = none.unwrap_or("31 minutos");
-  expect(result).toBe("31 minutos");
+describe("Testing `unwrap_or_else` method", () => {
+  test("`None` should call the `func` parameter and return its value", () => {
+    const none = None<number>();
+    const result = none.unwrap_or_else(() => 0xe0218a);
+    expect(result).toBe(0xe0218a);
+  });
+
+  test("`Some` should just return its own value", () => {
+    const some = Some(0);
+    const result = some.unwrap_or_else(() => 1);
+    expect(result).toBe(0);
+  });
 });
 
-test("`unwrap_or` should not be called ", () => {
-  const some = Some("Mr. Trance");
-  const result = some.unwrap_or("Esteman");
-  expect(result).toBe("Mr. Trance");
-});
+describe("Testing `zip` method", () => {
+  test("`Some` and `None` should return `None`", () => {
+    const some = Some(1);
+    const none = None<string>();
+    expect(some.zip(none)).toEqual(None());
+  });
 
-test("`unwrap_or_else` should be called ", () => {
-  const none = None<number>();
-  const result = none.unwrap_or_else(() => 0xe0218a);
-  expect(result).toBe(0xe0218a);
-});
+  test("`None` and `Some` should return `None`", () => {
+    const none = None<number[]>();
+    const some = Some("Tengo que llenar esto con algo");
+    expect(none.zip(some)).toEqual(None());
+  });
 
-test("`unwrap_or_else` should not be called ", () => {
-  const some = Some(0);
-  const result = some.unwrap_or_else(() => 1);
-  expect(result).toBe(0);
-});
+  test("`Some` and `Some` should return `Some` containing the values of the two previous `Some`", () => {
+    const some1 = Some("好きな日本語");
+    const some2 = Some(135);
+    expect(some1.zip(some2)).toEqual(Some(["好きな日本語", 135]));
+  });
 
-test("testing `zip` method", () => {
-  let val1 = Some(1);
-  let val2 = None<string>();
-  expect(val1.zip(val2)).toEqual(None());
-
-  val1 = None();
-  val2 = Some("thing");
-  expect(val1.zip(val2)).toEqual(None());
-
-  val1 = Some(0+0+7);
-  val2 = Some("Agente");
-  expect(val1.zip(val2)).toEqual(Some([7, "Agente"]));
-
-  val1 = None();
-  val2 = None();
-  expect(val1.zip(val2)).toEqual(None());
+  test("`None` and `None` should return `None`", () => {
+    const none1 = None();
+    const none2 = None();
+    expect(none1.zip(none2)).toEqual(None());
+  });
 });
