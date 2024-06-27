73ec5fad9ba6c2e6ae0038b23956a7f40b773d61
Shoghy Martinez <54950187+Shoghy@users.noreply.github.com>
2024-06-27 10:24:25 -0400

    ♻`Mutex` from function to class

--- src/mutex.ts
+++ src/mutex.ts
@@ -1,62 +1,103 @@
-export function Mutex<T>(value: T) {
-  let currentLocker: Promise<unknown>;
-  let lockers_count = 0;
-  const unlockers: {
-    [key: symbol]: () => void,
+import { catch_unwind, panic } from "./panic";
+import { type Result } from "./result";
+
+export class Mutex<T> {
+  private readonly get: () => T;
+  private readonly set: (val: T) => void;
+
+  private unlockers: {
+    [key: symbol]: () => void
   } = {};
+  private lockers_count = 0;
+  private current_locker!: Promise<unknown>;
 
-  return {
-    is_locked() {
-      return lockers_count > 0;
-    },
-    /**
-     * Unlocks the Mutex, without needing the lockers.
-     * ## This function can be error prone its used is not recommended
-     */
-    forced_unlock() {
-      const keys = Object.getOwnPropertySymbols(unlockers);
-      for(const key of keys){
-        unlockers[key]();
+  constructor(value: T) {
+    this.get = () => value;
+    this.set = (val: T) => {
+      value = val;
+    };
+  }
+
+  get_lockers_count(): number {
+    return this.lockers_count;
+  }
+
+  is_locked(): boolean {
+    return this.lockers_count > 0;
+  }
+
+  /**
+   * Unlocks the Mutex, without needing the lockers.
+   * ## This function can be error prone its used is not recommended
+   */
+  forced_unlock() {
+    const keys = Object.getOwnPropertySymbols(this.unlockers);
+    for (const key of keys) {
+      this.unlockers[key]();
+    }
+  }
+
+  async lock(): Promise<MutexGuard<T>> {
+    this.lockers_count += 1;
+
+    const prev_locker = this.current_locker;
+
+    let resolve_promise: () => void = () => { };
+
+    this.current_locker = new Promise((resolve) => {
+      const unlocker_key = Symbol();
+      this.unlockers[unlocker_key] = () => mutex_guard.unlock();
+
+      resolve_promise = () => {
+        this.lockers_count -= 1;
+        delete this.unlockers[unlocker_key];
+        resolve(undefined);
+      };
+    });
+
+    const mutex_guard = new MutexGuard(this.get, this.set, resolve_promise);
+
+    await prev_locker;
+
+    return mutex_guard;
+  }
+}
+
+export class MutexGuard<T> {
+  get: () => T;
+  set: (val: T) => void;
+  unlock: () => void;
+  is_locked: () => boolean;
+
+  constructor(get: () => T, set: (val: T) => void, resolver: () => void) {
+    this.get = () => get();
+    this.set = (val: T) => set(val);
+
+    let is_locked = true;
+
+    this.is_locked = () => is_locked;
+
+    this.unlock = () => {
+      if (!is_locked) {
+        panic("Calling `unlock` when `MutexGuard` has been unlocked");
       }
-    },
-    async lock() {
-      lockers_count += 1;
+      is_locked = false;
 
-      const prevLocker = currentLocker;
+      get = () => panic("Calling `get` when `MutexGuard` has been unlocked");
+      set = () => panic("Calling `set` when `MutexGuard` has been unlocked");
+      resolver();
+    };
+  }
 
-      let resolvePromise: (v?: unknown) => void;
-      currentLocker = new Promise((resolve) => {
-        resolvePromise = resolve;
-      });
+  try_get(): Result<T, Error> {
+    return catch_unwind(this.get);
+  }
 
-      let get = () => value;
+  try_set(value: T): Result<void, Error> {
+    return catch_unwind(() => this.set(value));
+  }
 
-      let set = (val: T) => {
-        value = val;
-      };
-
-      const unlockerKey = Symbol();
-
-      let unlock = () => {
-        get = undefined as never;
-        set = undefined as never;
-        unlock = undefined as never;
-        delete unlockers[unlockerKey];
-        lockers_count -= 1;
-        resolvePromise();
-      };
-
-      unlockers[unlockerKey] = unlock;
-
-      await prevLocker;
-
-      const mutexGuard = {
-        get: () => get(),
-        set: (val: T) => set(val),
-        unlock: () => unlock(),
-      };
-
-      return mutexGuard;
-    },
-  };
+  try_unlock(): Result<void, Error> {
+    return catch_unwind(this.unlock);
+  }
 }
