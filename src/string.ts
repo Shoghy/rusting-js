9050abe8e2d320003d40bb4e59778f47d5d4b4ca
Shoghy Martinez <54950187+Shoghy@users.noreply.github.com>
2024-07-16 14:58:25 -0400

    ✨Adding more methods for `RuString`

--- src/string.ts
+++ src/string.ts
@@ -6,22 +6,27 @@ const text_decoder = new TextDecoder();
 const vec_symbol = Symbol("vec");
 
 export class RString {
-  private [vec_symbol]: Uint8Array & {buffer: ArrayBuffer};
+  private [vec_symbol]: Uint8Array & { buffer: ArrayBuffer };
 
   constructor() {
-    const buffer = new ArrayBuffer(0, {maxByteLength: Math.pow(2, 31) - 1});
-    this[vec_symbol] = new Uint8Array(buffer) as Uint8Array & {buffer: ArrayBuffer};
+    const buffer = new ArrayBuffer(0, { maxByteLength: Math.pow(2, 31) - 1 });
+    this[vec_symbol] = new Uint8Array(buffer) as Uint8Array & { buffer: ArrayBuffer };
   }
 
-  static from<T extends ArrayLike<string>>(arr_like: T) {
+  static from(arr_like: ArrayLike<string>) {
     const bytes: number[] = [];
-    for (let i = 0; i < arr_like.length; ++i) {
-      const val = arr_like[i];
 
-      if (typeof val !== "string") {
-        panic("`arr_like` contiene valores que no son `string`");
+    if (typeof arr_like === "string") {
+      bytes.push(...text_encoder.encode(arr_like));
+    } else {
+      for (let i = 0; i < arr_like.length; ++i) {
+        const val = arr_like[i];
+
+        if (typeof val !== "string") {
+          panic("`arr_like` contiene valores que no son `string`");
+        }
+        bytes.push(...text_encoder.encode(val));
       }
-      bytes.push(...text_encoder.encode(val));
     }
 
     const self = new RString();
@@ -31,11 +36,39 @@ export class RString {
     return self;
   }
 
-  len(): number{
+  len(): number {
     return this[vec_symbol].length;
   }
 
-  toString(){
+  toString(): string {
     return text_decoder.decode(this[vec_symbol]);
+  }
+
+  push_str(str: ArrayLike<string> | RString): void {
+    if (!(str instanceof RString)) {
+      str = RString.from(str);
+    }
+
+    const vec = this[vec_symbol];
+    const prev_length = vec.length;
+    vec.buffer.resize(prev_length + str.len());
+    vec.set(str[vec_symbol], prev_length);
+  }
+
+  as_bytes(): Uint8Array {
+    return this[vec_symbol].slice();
+  }
+
+  clear() {
+    const vec = this[vec_symbol];
+    vec.buffer.resize(0);
+  }
+
+  is_empty() {
+    return this[vec_symbol].length === 0;
+  }
+
+  capacity() {
+    return this[vec_symbol].byteLength;
   }
 }
