fee196f998afe3eed1db990cf7cc3e1aca5583b7
Shoghy Martinez <54950187+Shoghy@users.noreply.github.com>
2024-11-19 10:11:09 -0400

    🔧Updating rule

--- eslint.config.js
+++ eslint.config.js
@@ -21,7 +21,7 @@ export default [
         { varsIgnorePattern: "^_" },
       ],
       "comma-dangle": ["error", "always-multiline"],
-      "no-shadow": "warn",
+      "@typescript-eslint/no-shadow": "warn",
     },
   },
 ];
