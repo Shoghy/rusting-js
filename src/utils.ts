d5f06ac6d5835809e9d61585d71e5c39b45ba153
Shoghy Martinez <54950187+Shoghy@users.noreply.github.com>
2024-12-27 11:41:30 -0400

    🔥Removing useless code

--- src/utils.ts
+++ src/utils.ts
@@ -1,59 +1,10 @@
 import type { Result } from "./enums/result";
 import { catch_unwind, panic } from "./panic";
+
 export function StaticImplements<T>() {
   return <U extends T>(constructor: U) => {
     constructor;
   };
-}
-
-export function CloneValue<T>(obj: T): T {
-  if (typeof obj !== "object" || obj === null) {
-    return obj;
-  }
-
-  if (!Array.isArray(obj)) {
-    return Object.assign(Object.create(Object.getPrototypeOf(obj)), obj);
-  }
-
-  const arrClone: unknown[] = [];
-  const checkedObjs: object[] = [obj];
-  const clonedObjs: object[] = [arrClone];
-
-  function RecursiveChecker(value: unknown): unknown {
-    if (typeof value !== "object") {
-      return value;
-    }
-
-    const index = checkedObjs.indexOf(value as object);
-    if (index > -1) {
-      return clonedObjs[index];
-    }
-
-    checkedObjs.push(value as object);
-    if (!Array.isArray(value)) {
-      const clone = Object.assign(
-        Object.create(Object.getPrototypeOf(value)),
-        value,
-      );
-      clonedObjs.push(clone);
-      return clone;
-    }
-
-    const rArrClone: unknown[] = [];
-
-    for (const val of value) {
-      rArrClone.push(RecursiveChecker(val));
-    }
-
-    clonedObjs.push(rArrClone);
-    return rArrClone;
-  }
-
-  for (const val of obj) {
-    arrClone.push(RecursiveChecker(val));
-  }
-
-  return arrClone as T;
 }
 
 export function CopyTo(dest: object, src: object): void {
@@ -64,8 +15,8 @@ export function CopyTo(dest: object, src: object): void {
 export interface DeferObject {
   [Symbol.dispose](): void;
   [Symbol.asyncDispose](): Promise<void>;
-  resolve(): Promise<void>;
 }
+
 /**
  * The `func` parameter is executed when the
  * function that called `defer` returns
@@ -85,25 +36,6 @@ export interface DeferObject {
  * example();
  * //Hello young lady
  * //AEUGH
- * ```
- *
- * If your current enviroment doesn't support the `using` keyword
- * you can use the method `resolve` from the returned value.
- *
- * ```ts
- * function exemple() {
- *   const d1 = defer(() => {
- *     console.log("Sorry the machine isn't working.");
- *   });
- *
- *   console.log("I will like an Ice Cream.");
- *   d1.resolve();
- * }
- *
- * exemple();
- * //I will like an Ice Cream.
- * //Sorry the machine isn't working.
- * ```
  */
 export function defer(func: () => unknown): DeferObject {
   return {
@@ -113,13 +45,6 @@ export function defer(func: () => unknown): DeferObject {
 
     async [Symbol.asyncDispose]() {
       await func();
-    },
-
-    async resolve() {
-      const value = func();
-      if (value instanceof Promise) {
-        await value;
-      }
     },
   };
 }
